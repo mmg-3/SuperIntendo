4b4766d5fe09bce40c8304a1c5647cfca5cd86b7
Manuel Bugallo <marvinody@gmail.com>
2019-06-27 18:04:45 -0400

    feat: finished schema associations

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,5 +1,23 @@
 const User = require('./user')
+const Building = require('./building')
+const Apartment = require('./apartment')
+const Owner = require('./owner')
+const Resident = require('./resident')
 
+Owner.belongsTo(User)
+User.hasOne(Owner)
+
+Resident.belongsTo(User)
+User.hasOne(Resident)
+
+Building.hasMany(Apartment)
+Apartment.belongsTo(Building)
+
+Building.belongsTo(Owner)
+Owner.hasMany(Building)
+
+Apartment.hasMany(Resident)
+Resident.belongsTo(Apartment)
 /**
  * If we had any associations to make, this would be a great place to put them!
  * ex. if we had another model called BlogPost, we might say:
