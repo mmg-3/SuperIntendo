b97ae5e20cee631a2a9cc023c1a046b6b81d790d
Vivian T <vivianxtong@gmail.com>
2019-07-02 11:15:39 -0400

    refactor: add phoneNumber and imageUrl to seed and tests

--- server/api/tests/owner.spec.js
+++ server/api/tests/owner.spec.js
@@ -51,12 +51,18 @@ describe('Owner routes', () => {
       resident = await Resident.create({
         firstName: 'Cody',
         lastName: 'daPug',
-        userId: user.id
+        userId: user.id,
+        phoneNumber: '111-111-1111',
+        imageUrl:
+          'https://user-images.githubusercontent.com/12876798/38030875-d3166276-3267-11e8-96d9-309aa8cf008b.png'
       })
       resident2 = await Resident.create({
         firstName: 'user',
         lastName: 'coinstance',
-        userId: user2.id
+        userId: user2.id,
+        phoneNumber: '222-222-2222',
+        imageUrl:
+          'https://globalcoinreport.com/wp-content/uploads/2018/03/cropped-favicon.png'
       })
       owner = await Owner.create({
         userId: user3.id
