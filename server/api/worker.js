7ddc29645ebbd69e4af39fe69fe5a06ea45e42d4
Vivian T <vivianxtong@gmail.com>
2019-07-02 12:54:52 -0400

    refactor(server/api/workers): remove all workers route

--- server/api/worker.js
+++ server/api/worker.js
@@ -28,38 +28,9 @@ const isWorker = async (req, res, next) => {
     next(err)
   }
 }
-const isOwner = async (req, res, next) => {
-  try {
-    const owner = await Owner.findOne({
-      where: {
-        userId: req.user.id
-      },
-      include: [Building]
-    })
-    if (owner) {
-      req.user.ownerId = owner.id
-      req.user.buildingIds = owner.buildings.map(building => building.id)
-      next()
-    } else {
-      res.sendStatus(401)
-    }
-  } catch (err) {
-    next(err)
-  }
-}
 
 router.use(isLoggedIn)
 router.use(isWorker)
-router.use(isOwner)
-router.get('/', async (req, res, next) => {
-  try {
-    const workers = await Worker.findAll()
-    res.json(workers)
-    console.log(workers)
-  } catch (err) {
-    next(err)
-  }
-})
 
 router.get('/:workerId', async (req, res, next) => {
   try {
