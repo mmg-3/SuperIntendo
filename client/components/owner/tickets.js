9933866f7ca0346d2578dddf5fa0eb18ca916ca3
Vivian T <vivianxtong@gmail.com>
2019-07-02 18:57:15 -0400

    feat(components/owners/tickets): owners can view pending tickets and confirmed tickets

--- client/components/owner/tickets.js
+++ client/components/owner/tickets.js
@@ -1,6 +1,11 @@
 import React, {useEffect} from 'react'
 import {connect} from 'react-redux'
-import {getABuilding, getWorkers} from '../../store/owner'
+import {
+  getABuilding,
+  getWorkers,
+  assignWorker,
+  closeTicket
+} from '../../store/owner'
 import WorkerSelection from './worker-selection'
 
 export const Tickets = props => {
@@ -13,14 +18,40 @@ export const Tickets = props => {
   if (!props.id) {
     return <div>Loading...</div>
   }
+  const handleClose = id => {
+    props.closeTicket(id)
+  }
   const tickets = props.apartments.flatMap(apartment => apartment.tickets)
   return (
     <div>
-      <div>{props.address} Tickets</div>
+      <h2>{props.address} Tickets</h2>
+      <h3>Pending Tickets To Assign</h3>
       <ul>
-        {tickets.map(ticket => (
-          <li key={ticket.id}>
-            {ticket.issue} <WorkerSelection />
+        {tickets.filter(ticket => ticket.status === 'pending').map(tix => (
+          <li key={tix.id}>
+            {tix.issue} <br />
+            {tix.status} <WorkerSelection />
+            <button type="submit">Assign Worker</button>
+          </li>
+        ))}
+        {/* {tickets.map(ticket => (
+            <li key={ticket.id}>
+              {ticket.issue}
+              <br></br>
+              {ticket.status}
+              <WorkerSelection />
+              <button type="submit">Assign Worker</button>
+            </li>
+          ))} */}
+      </ul>
+      <h3>Confirmed Tickets To Close</h3>
+      <ul>
+        {tickets.filter(ticket => ticket.status === 'confirmed').map(tix => (
+          <li key={tix.id}>
+            {tix.issue}
+            <button type="submit" onClick={props.handleClose}>
+              Close Ticket
+            </button>
           </li>
         ))}
       </ul>
@@ -34,7 +65,8 @@ const mapStateToProps = state => ({
 
 const mapDispatchToProps = dispatch => ({
   getABuilding: id => dispatch(getABuilding(id)),
-  getWorkers: () => dispatch(getWorkers())
+  getWorkers: () => dispatch(getWorkers()),
+  closeTicket: id => duspatch(closeTicket(id))
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(Tickets)
