dbca2efdcd764e3ea6979f0ba50c4d14644e6f66
Constance Kang <linweikang1203@gmail.com>
2019-07-03 14:23:55 -0400

    profile form work in progress

--- client/components/resident/profile-form.js
+++ client/components/resident/profile-form.js
@@ -1,61 +1,89 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import {makeOnChange, TextField} from './utils'
+import {updateProfile} from '../store/resident'
+import {connect} from 'react-redux'
 /**
  * COMPONENT
  */
 
 export const ProfileForm = props => {
-  const [firstName, setFirstName] = useState(props.firstName)
-  const [lastName, setLastName] = useState(props.lastName)
-  const [phoneNumber, setPhoneNumber] = useState(props.phoneNumber)
-  const [email, setEmail] = useState(props.email)
-  const [address, setAddress] = useState(props.address)
-  const [photoUrl, _] = useState(props.photoUrl)
+  const [firstName, setFirstName] = useState(props.resident.firstName)
+  const [lastName, setLastName] = useState(props.resident.lastName)
+  const [phoneNumber, setPhoneNumber] = useState(props.resident.phoneNumber)
+  const [maillingAddress, setMaillingAddress] = useState(
+    props.resident.maillingAddress
+  )
+
+  const onSubmit = event => {
+    event.preventDefault()
+    props.updateProfile({
+      firstName,
+      lastName,
+      phoneNumber,
+      maillingAddress
+    })
+    setFirstName(props.resident.firstName)
+    setLastName(props.resident.lastName)
+    setPhoneNumber(props.resident.phoneNumber)
+    setMaillingAddress(props.resident.maillingAddress)
+  }
 
   return (
-    <form onSubmit={props.handleSubmit}>
-      <TextField
-        name="first-name"
-        label="First Name"
-        type="text"
-        value={firstName}
-        onChange={makeOnChange(setFirstName)}
-      />
+    <div>
+      <image src={props.resident.photoUrl} />
+      <form onSubmit={onSubmit}>
+        <TextField
+          name="first-name"
+          label="First Name"
+          type="text"
+          value={firstName}
+          onChange={makeOnChange(setFirstName)}
+        />
 
-      <TextField
-        name="last-name"
-        label="Last Name"
-        type="text"
-        value={lastName}
-        onChange={makeOnChange(setLastName)}
-      />
+        <TextField
+          name="last-name"
+          label="Last Name"
+          type="text"
+          value={lastName}
+          onChange={makeOnChange(setLastName)}
+        />
 
-      <TextField
-        name="phone-number"
-        label="Phone Number"
-        type="phone"
-        value={phoneNumber}
-        onChange={makeOnChange(setPhoneNumber)}
-      />
+        <TextField
+          name="phone-number"
+          label="Phone Number"
+          type="phone"
+          value={phoneNumber}
+          onChange={makeOnChange(setPhoneNumber)}
+        />
 
-      <TextField
-        name="email"
-        label="Email"
-        type="text"
-        value={email}
-        onChange={makeOnChange(setEmail)}
-      />
+        <TextField
+          name="address"
+          label="Mailing Address"
+          type="text"
+          placeholder="alternative address if you have one"
+          value={maillingAddress}
+          onChange={makeOnChange(setMaillingAddress)}
+        />
 
-      <TextField
-        name="address"
-        label="Address"
-        type="text"
-        value={address}
-        onChange={makeOnChange(setAddress)}
-      />
-
-      <button type="submit">Save</button>
-      <button type="reset">Cancel</button>
-    </form>
+        <button type="submit">Save</button>
+        <button type="reset">Cancel</button>
+      </form>
+    </div>
   )
 }
+
+const mapStateToProps = state => {
+  return {
+    resident: state.resident
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    updateProfile: updatedSelf => {
+      dispatch(updateProfile(updatedSelf))
+    }
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(ProfileForm)
