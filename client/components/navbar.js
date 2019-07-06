5f3cc71a910b0229799b7f8cab09a461b3a542a5
Constance Kang <linweikang1203@gmail.com>
2019-07-05 22:58:31 -0400

    updated: navbar

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -3,7 +3,9 @@ import React from 'react'
 import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
 import {logout} from '../store'
-const Navbar = ({handleClick, isLoggedIn, isResident, isOwner, isWorker}) => {
+import {Navbar} from 'react-bulma-components/full'
+
+const NavBar = ({handleClick, isLoggedIn, isResident, isOwner, isWorker}) => {
   const navbar = (
     <div>
       <Link to="/" />
@@ -11,26 +13,181 @@ const Navbar = ({handleClick, isLoggedIn, isResident, isOwner, isWorker}) => {
   )
   return (
     <div>
-      <h1>BOILERMAKER</h1>
-      <nav>
-        {isLoggedIn ? (
-          <div>
-            {/* The navbar will show these links after you log in */}
-            <Link to="/home">Home</Link>
-            <a href="#" onClick={handleClick}>
-              Logout
-            </a>
-            {isOwner && <Link to="/buildings">Buildings</Link>}
-          </div>
-        ) : (
-          <div>
-            {/* The navbar will show these links before you log in */}
-            <Link to="/login">Login</Link>
-            <Link to="/signup">Sign Up</Link>
+      <nav className="navbar" role="navigation" aria-label="main navigation">
+        <div className="navbar-brand">
+          <a className="navbar-item" href="/">
+            <img
+              src="https://bulma.io/images/bulma-logo.png"
+              width="112"
+              height="28"
+            />
+          </a>
+
+          <a
+            role="button"
+            className="navbar-burger burger"
+            aria-label="menu"
+            aria-expanded="false"
+            data-target="navbarBasicExample"
+          >
+            <span aria-hidden="true" />
+            <span aria-hidden="true" />
+            <span aria-hidden="true" />
+          </a>
+        </div>
+        {isLoggedIn &&
+          isOwner && (
+            <div id="navbarBasicExample" className="navbar-menu">
+              <div className="navbar-start">
+                <a className="navbar-item">Home</a>
+
+                <a className="navbar-item" href="/buildings">
+                  Buildings
+                </a>
+                <div className="navbar-item has-dropdown is-hoverable">
+                  <a className="navbar-link">Tickets</a>
+
+                  <div className="navbar-dropdown">
+                    <a className="navbar-item">Actions Needed</a>
+                    <a className="navbar-item">Closed Tickets</a>
+                  </div>
+                </div>
+
+                <div className="navbar-item has-dropdown is-hoverable">
+                  <a className="navbar-link">Users</a>
+
+                  <div className="navbar-dropdown">
+                    <a className="navbar-item">Residents</a>
+                    <a className="navbar-item">Workers</a>
+                    <a className="navbar-item">Admin</a>
+                  </div>
+                </div>
+                <a className="navbar-item">FAQs</a>
+              </div>
+
+              <div className="navbar-end">
+                <div className="navbar-item">
+                  <div className="buttons">
+                    <a
+                      className="button is-light"
+                      href="#"
+                      onClick={handleClick}
+                    >
+                      Log out
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          )}
+        {isLoggedIn &&
+          isResident && (
+            <div id="navbarBasicExample" className="navbar-menu">
+              <div className="navbar-start">
+                <a className="navbar-item">Home</a>
+
+                <a className="navbar-item">My Account</a>
+                <div className="navbar-item has-dropdown is-hoverable">
+                  <a className="navbar-link">Tickets</a>
+
+                  <div className="navbar-dropdown">
+                    <a className="navbar-item">Current Ticket</a>
+                    <a className="navbar-item">Archived Tickets</a>
+                  </div>
+                </div>
+                <a className="navbar-item">News</a>
+                <a className="navbar-item">FAQs</a>
+              </div>
+
+              <div className="navbar-end">
+                <div className="navbar-item">
+                  <div className="buttons">
+                    <a
+                      className="button is-light"
+                      href="#"
+                      onClick={handleClick}
+                    >
+                      Log out
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          )}
+        {isLoggedIn &&
+          isWorker && (
+            <div id="navbarBasicExample" className="navbar-menu">
+              <div className="navbar-start">
+                <a className="navbar-item">Home</a>
+
+                <a className="navbar-item">My Account</a>
+                <div className="navbar-item has-dropdown is-hoverable">
+                  <a className="navbar-link">Tickets</a>
+
+                  <div className="navbar-dropdown">
+                    <a className="navbar-item">Current Ticket</a>
+                    <a className="navbar-item">Archived Tickets</a>
+                  </div>
+                </div>
+                <a className="navbar-item">Payment</a>
+                <a className="navbar-item">FAQs</a>
+              </div>
+
+              <div className="navbar-end">
+                <div className="navbar-item">
+                  <div className="buttons">
+                    <a
+                      className="button is-light"
+                      href="#"
+                      onClick={handleClick}
+                    >
+                      Log out
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          )}
+        {!isLoggedIn && (
+          <div id="navbarBasicExample" className="navbar-menu">
+            <div className="navbar-start">
+              <a className="navbar-item">Home</a>
+
+              <a className="navbar-item">Pricing</a>
+              <div className="navbar-item has-dropdown is-hoverable">
+                <a className="navbar-link">Documentation</a>
+
+                <div className="navbar-dropdown">
+                  <a className="navbar-item">For Owner</a>
+                  <a className="navbar-item">For Resident</a>
+                  <a className="navbar-item">For Worker</a>
+                </div>
+              </div>
+              <div className="navbar-item has-dropdown is-hoverable">
+                <a className="navbar-link">More</a>
+
+                <div className="navbar-dropdown">
+                  <a className="navbar-item">About Us</a>
+                  <a className="navbar-item">Contact</a>
+                </div>
+              </div>
+            </div>
+
+            <div className="navbar-end">
+              <div className="navbar-item">
+                <div className="buttons">
+                  <a className="button is-primary" href="/signup">
+                    <strong>Sign up</strong>
+                  </a>
+                  <a className="button is-light" href="/login">
+                    Log in
+                  </a>
+                </div>
+              </div>
+            </div>
           </div>
         )}
       </nav>
-      <hr />
     </div>
   )
 }
@@ -55,7 +212,7 @@ const mapDispatch = dispatch => {
   }
 }
 
-export default connect(mapState, mapDispatch)(Navbar)
+export default connect(mapState, mapDispatch)(NavBar)
 
 /**
  * PROP TYPES
