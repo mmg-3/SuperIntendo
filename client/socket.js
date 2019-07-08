678f664c167b15159be6710f821408814f9a7cf9
Manuel Bugallo <marvinody@gmail.com>
2019-07-08 15:19:16 -0400

    feat: client notification and reload done for worker jobs

--- client/socket.js
+++ client/socket.js
@@ -1,9 +1,22 @@
 import io from 'socket.io-client'
-
+import {canNotify, makeNotification} from './notify'
+import store from './store'
+import {getTickets} from './store/worker'
 const socket = io(window.location.origin)
+const isWorker = store => {
+  const user = store.getState().user
+  return user.isWorker && user.isWorkerVerified
+}
 
 socket.on('connect', () => {
   console.log('Connected!')
+
+  socket.on('job_open', () => {
+    if (isWorker(store) && canNotify()) {
+      makeNotification('a new job just opened, be sure to check it out')
+      store.dispatch(getTickets())
+    }
+  })
 })
 
 export default socket
