cb5329b78bc49f9fcf556e6432d9f3ec9f9e5ed4
Vivian T <vivianxtong@gmail.com>
2019-07-09 07:18:06 -0400

    feat: add single-resident component to render single verified resident profile for owner

---
+++ client/components/owner/single-resident.js
@@ -0,0 +1,80 @@
+import React, {useEffect} from 'react'
+import {connect} from 'react-redux'
+import {getABuilding} from '../../store/owner'
+
+export const SingleResident = props => {
+  useEffect(() => {
+    props.getABuilding(props.match.params.id)
+  }, [])
+  if (!props.selectedBuilding.id) {
+    return <div>Loading...</div>
+  }
+  const resident = props.selectedBuilding.apartments
+    .map(apartment =>
+      apartment.residents.map(res => ({...res, unit: apartment.unitNumber}))
+    )
+    .flat()
+    .filter(res => +res.id === +props.match.params.residentId)[0]
+
+  const phoneNumber = phoneStr => {
+    return (
+      phoneStr[0] +
+      phoneStr[1] +
+      phoneStr[2] +
+      '-' +
+      phoneStr[3] +
+      phoneStr[4] +
+      phoneStr[5] +
+      '-' +
+      phoneStr[6] +
+      phoneStr[7] +
+      phoneStr[8] +
+      phoneStr[9]
+    )
+  }
+
+  const alternativeAddress = address => {
+    if (address.length > 0) {
+      return (
+        address +
+        ', ' +
+        resident.city +
+        ', ' +
+        resident.state +
+        ', ' +
+        resident.zipcode
+      )
+    }
+    return ''
+  }
+  return (
+    <div>
+      <h3>
+        {resident.firstName} {resident.lastName}'s Profile
+      </h3>
+      <h4>
+        {props.selectedBuilding.address} - Apartment {resident.unit}
+      </h4>
+      <img src={resident.photoUrl} />
+      <div>
+        <ul>
+          <li>First Name: {resident.firstName}</li>
+          <li>Last Name: {resident.lastName}</li>
+          <li>Phone Number: {phoneNumber(resident.phoneNumber)}</li>
+          <li>Email Address: {resident.user.email}</li>
+          <li>Alternative Address: {alternativeAddress(resident.address1)}</li>
+          <li>Alternative Address: {alternativeAddress(resident.address2)}</li>
+          <li />
+        </ul>
+      </div>
+    </div>
+  )
+}
+const mapStateToProps = state => ({
+  selectedBuilding: state.owner.selectedBuilding
+})
+
+const mapDispatchToProps = dispatch => ({
+  getABuilding: id => dispatch(getABuilding(id))
+})
+export default connect(mapStateToProps, mapDispatchToProps)(SingleResident)
