1953501fb7c78e2a2be6a7a6d490c883c96cce1e
Constance Kang <linweikang1203@gmail.com>
2019-07-08 21:44:14 -0400

    owner/building/residents-verified and unverified lists

---
+++ client/components/owner/single-building-residents.js
@@ -0,0 +1,150 @@
+import React from 'react'
+import {connect} from 'react-redux'
+import {Link} from 'react-router-dom'
+
+export const SingleBuildingResidents = props => {
+  return (
+    <div>
+      <div className="page-tab">
+        <div className="tabs is-small">
+          <ul>
+            <li>
+              <a>
+                <span className="icon is-small">
+                  <i className="fas fa-exclamation-circle" aria-hidden="true" />
+                </span>
+                <span>Vacancy</span>
+              </a>
+            </li>
+            <li className="is-active">
+              <a>
+                <span className="icon is-small">
+                  <i className="fas fa-users" aria-hidden="true" />
+                </span>
+                <span>Residents</span>
+              </a>
+            </li>
+            <li>
+              <a>
+                <span className="icon is-small">
+                  <i className="fas fa-clipboard-check" aria-hidden="true" />
+                </span>
+                <span>Tickets</span>
+              </a>
+            </li>
+            <li>
+              <a>
+                <span className="icon is-small">
+                  <i className="far fa-newspaper" aria-hidden="true" />
+                </span>
+                <span>News</span>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="body">
+        {props.unverifiedResidents.length > 0 && (
+          <div>
+            <h3 className="title is-6">
+              Unverified Residents{' '}
+              <span className="tag is-warning">Warning</span>
+            </h3>
+
+            <table className="table">
+              <thead>
+                <tr>
+                  <th>Apartment No.</th>
+                  <th>First Name</th>
+                  <th>Last Name</th>
+                  <th>Profile</th>
+                  <th>Action</th>
+                </tr>
+              </thead>
+              {props.unverifiedResidents.map(resident => (
+                <tbody key={resident.id}>
+                  <Link
+                    to={`/buildings/${props.id}/residents/${resident.id}`}
+                  />
+                  <tr>
+                    <th>{resident.number}</th>
+                    <td>{resident.firstName}</td>
+                    <td>{resident.lastName}</td>
+                    <td>
+                      <Link
+                        to={`/buildings/${props.id}/residents/${resident.id}`}
+                      >
+                        details
+                      </Link>
+                    </td>
+                    <td>
+                      <a
+                        className="button is-success"
+                        onClick={() => props.verifyUser(props.id, resident.id)}
+                      >
+                        <span className="icon is-small">
+                          <i className="fas fa-check" />
+                        </span>
+                        <span>Approve</span>
+                      </a>
+
+                      <a
+                        className="button is-danger is-outlined"
+                        onClick={() => props.rejectUser(props.id, resident.id)}
+                      >
+                        <span>Reject</span>
+                        <span className="icon is-small">
+                          <i className="fas fa-times" />
+                        </span>
+                      </a>
+                    </td>
+                  </tr>
+                </tbody>
+              ))}
+            </table>
+          </div>
+        )}
+        <br />
+        {props.verifiedResidents.length > 0 && (
+          <div>
+            <h3 className="title is-6">
+              Verified Residents <span className="tag is-success">Ok</span>
+            </h3>
+            <table className="table">
+              <thead>
+                <tr>
+                  <th>Apartment No.</th>
+                  <th>First Name</th>
+                  <th>Last Name</th>
+                  <th />
+                </tr>
+              </thead>
+
+              {props.verifiedResidents.map(resident => (
+                <tbody key={resident.id}>
+                  <Link
+                    to={`/buildings/${props.id}/residents/${resident.id}`}
+                  />
+                  <tr>
+                    <th>{resident.number}</th>
+                    <td>{resident.firstName}</td>
+                    <td>{resident.lastName}</td>
+                    <td>
+                      <Link
+                        to={`/buildings/${props.id}/residents/${resident.id}`}
+                      >
+                        details
+                      </Link>
+                    </td>
+                  </tr>
+                </tbody>
+              ))}
+            </table>
+          </div>
+        )}
+      </div>
+    </div>
+  )
+}
+
+export default SingleBuildingResidents
--- client/components/owner/single-building.js
+++ client/components/owner/single-building.js
@@ -3,6 +3,8 @@ import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
 import {getABuilding, rejectUser, verifyUser} from '../../store/owner'
 import NewsNew from '../owner/news-new'
+import SingleBuildingVacancy from './single-building-vacancy'
+import SingleBuildingResidents from './single-building-residents'
 
 export const SingleBuilding = props => {
   useEffect(() => {
@@ -31,78 +33,18 @@ export const SingleBuilding = props => {
   console.log(residents)
   return (
     <div>
-      <div>
-        {props.address} - {numVacant} vacant apartment(s)
-      </div>
-      <div className="ticket-container">
-        <div>
-          <Link to={`/buildings/${props.id}/tickets`}>Tickets</Link>:
-        </div>
-        <div>
-          <ul>
-            {tickets.map(ticket => (
-              <li key={ticket.id}>
-                {ticket.status} - #{ticket.id} {ticket.issue}@{ticket.location}
-              </li>
-            ))}
-          </ul>
-        </div>
-        <div>
-          <Link to={`/buildings/${props.id}/news`}>News</Link>:
-          <NewsNew selectedBuilding={props} />
-        </div>
-        <div>
-          <ul>
-            {props.news.map(news => (
-              <li key={news.id}>
-                {news.title} - {news.body}
-              </li>
-            ))}
-          </ul>
-        </div>
-        {unverifiedResidents.length > 0 && (
-          <div>
-            Unverified Residents
-            <ul>
-              {unverifiedResidents.map(resident => (
-                <li key={resident.id}>
-                  {resident.number} - {resident.firstName} {resident.lastName} -{' '}
-                  <button
-                    type="button"
-                    onClick={() => props.verifyUser(props.id, resident.id)}
-                  >
-                    Approve
-                  </button>
-                  <button
-                    type="button"
-                    onClick={() => props.rejectUser(props.id, resident.id)}
-                  >
-                    Reject
-                  </button>
-                </li>
-              ))}
-            </ul>
-          </div>
-        )}
-        {verifiedResidents.length > 0 && (
-          <div>
-            <div>
-              <Link to={`/buildings/${props.id}/residents`}>
-                Verified Residents
-              </Link>:
-            </div>
-            <div>
-              <ul>
-                {verifiedResidents.map(resident => (
-                  <li key={resident.id}>
-                    {resident.number} - {resident.firstName} {resident.lastName}
-                  </li>
-                ))}
-              </ul>
-            </div>
-          </div>
-        )}
-      </div>
+      <SingleBuildingVacancy
+        address={props.address}
+        numVacant={numVacant}
+        apartments={props.apartments}
+      />
+      <SingleBuildingResidents
+        verifiedResidents={verifiedResidents}
+        unverifiedResidents={unverifiedResidents}
+        verifyUser={props.verifyUser}
+        rejectUser={props.rejectUser}
+        getABuilding={props.getABuilding}
+      />
     </div>
   )
 }
