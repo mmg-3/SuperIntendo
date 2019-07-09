477ae356b8b059d8b277f514491615d3a209be3f
Constance Kang <linweikang1203@gmail.com>
2019-07-08 22:27:09 -0400

    tickets:approval,close,on going,past

--- client/components/owner/tickets.js
+++ client/components/owner/tickets.js
@@ -20,75 +20,194 @@ export const Tickets = props => {
   const tickets = props.apartments.flatMap(apartment => apartment.tickets)
   return (
     <div>
-      <h2>{props.address} Tickets</h2>
-      <h3>Pending Tickets To Assign</h3>
-      <ul>
-        {tickets.filter(ticket => ticket.status === 'pending').map(tix => (
-          <li key={tix.id}>
-            {tix.issue}
-            <br />
-            {tix.status}
-            <button
-              type="submit"
-              onClick={evt => {
-                evt.preventDefault()
-                props.approveTicket(tix.id, props.match.params.id)
-              }}
-            >
-              Approve
-            </button>
-            <button
-              type="submit"
-              onClick={evt => {
-                evt.preventDefault()
-                props.rejectTicket(tix.id, props.match.params.id)
-              }}
-            >
-              Reject
-            </button>
-          </li>
-        ))}
-      </ul>
-      <h3>Confirmed Tickets To Close</h3>
-      <ul>
-        {tickets.filter(ticket => ticket.status === 'confirmed').map(tix => (
-          <li key={tix.id}>
-            {tix.issue}
-            <br />
-            {tix.status}
-            <button
-              type="submit"
-              onClick={evt => {
-                evt.preventDefault()
-                props.closeTicket(tix.id, props.match.params.id)
-              }}
-            >
-              Close Ticket
-            </button>
-          </li>
-        ))}
-      </ul>
-      <h3>Active Tickets Not Requiring Actions</h3>
-      <ul>
-        {tickets
-          .filter(
-            ticket =>
-              ticket.status === 'assigned' ||
-              ticket.status === 'in-progress' ||
-              ticket.status === 'finished'
-          )
-          .map(tix => (
-            <li key={tix.id}>
-              {tix.issue} <br /> {tix.status}
+      <div className="page-tab">
+        <div className="tabs is-small">
+          <ul>
+            <li>
+              <a>
+                <span className="icon is-small">
+                  <i className="fas fa-exclamation-circle" aria-hidden="true" />
+                </span>
+                <span>Vacancy</span>
+              </a>
             </li>
-          ))}
-      </ul>
-      <h3>Past Tickets</h3>
-      <ul>
-        {tickets
-          .filter(ticket => ticket.status === 'closed')
-          .map(tix => <li key={tix.id}>{tix.issue}</li>)}
-      </ul>
+            <li>
+              <a>
+                <span className="icon is-small">
+                  <i className="fas fa-users" aria-hidden="true" />
+                </span>
+                <span>Residents</span>
+              </a>
+            </li>
+            <li className="is-active">
+              <a>
+                <span className="icon is-small">
+                  <i className="fas fa-clipboard-check" aria-hidden="true" />
+                </span>
+                <span>Tickets</span>
+              </a>
+            </li>
+            <li>
+              <a>
+                <span className="icon is-small">
+                  <i className="far fa-newspaper" aria-hidden="true" />
+                </span>
+                <span>News</span>
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className="body">
+          <h3 className="title is-6">
+            Tickets Pending Approval{' '}
+            <span className="tag is-warning">need action</span>
+          </h3>
+          <table className="table">
+            <thead>
+              <tr>
+                <th>Ticket No.</th>
+                <th>Date</th>
+                <th>Location</th>
+                <th>Issue</th>
+                <th>Status</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            {tickets.filter(ticket => ticket.status === 'pending').map(tix => (
+              <tbody key={tix.id}>
+                <tr>
+                  <td>{tix.id}</td>
+                  <td>{tix.createdAt}</td>
+                  <td>{tix.location}</td>
+                  <td>{tix.issue}</td>
+                  <td>{tix.status}</td>
+                  <td>
+                    <button
+                      className="button-custom"
+                      type="submit"
+                      onClick={evt => {
+                        evt.preventDefault()
+                        props.approveTicket(tix.id, props.match.params.id)
+                      }}
+                    >
+                      Approve
+                    </button>
+                    <button
+                      className="button-custom"
+                      type="submit"
+                      onClick={evt => {
+                        evt.preventDefault()
+                        props.rejectTicket(tix.id, props.match.params.id)
+                      }}
+                    >
+                      Reject
+                    </button>
+                  </td>
+                </tr>
+              </tbody>
+            ))}
+          </table>
+          <h3 className="title is-6">
+            Tickets To Close <span className="tag is-warning">need action</span>
+          </h3>
+          <table className="table">
+            <thead>
+              <tr>
+                <th>Ticket No.</th>
+                <th>Date</th>
+                <th>Location</th>
+                <th>Issue</th>
+                <th>Status</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+
+            {tickets
+              .filter(ticket => ticket.status === 'confirmed')
+              .map(tix => (
+                <tbody key={tix.id}>
+                  <tr>
+                    <td>{tix.id}</td>
+                    <td>{tix.createdAt}</td>
+                    <td>{tix.location}</td>
+                    <td>{tix.issue}</td>
+                    <td>{tix.status}</td>
+                    <td>
+                      <button
+                        type="submit"
+                        onClick={evt => {
+                          evt.preventDefault()
+                          props.closeTicket(tix.id, props.match.params.id)
+                        }}
+                      >
+                        Close Ticket
+                      </button>
+                    </td>
+                  </tr>
+                </tbody>
+              ))}
+          </table>
+          <h3 className="title is-6">
+            On Going Tickets <span className="tag is-primary">in progress</span>
+          </h3>
+          <table className="table">
+            <thead>
+              <tr>
+                <th>Ticket No.</th>
+                <th>Date</th>
+                <th>Location</th>
+                <th>Issue</th>
+                <th>Status</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            {tickets
+              .filter(
+                ticket =>
+                  ticket.status === 'assigned' ||
+                  ticket.status === 'in-progress' ||
+                  ticket.status === 'finished'
+              )
+              .map(tix => (
+                <tbody key={tix.id}>
+                  <tr>
+                    <td>{tix.id}</td>
+                    <td>{tix.createdAt}</td>
+                    <td>{tix.location}</td>
+                    <td>{tix.issue}</td>
+                    <td>{tix.status}</td>
+                    <td>Not Required</td>
+                  </tr>
+                </tbody>
+              ))}
+          </table>
+          <h3 className="title is-6">
+            Past Tickets <span className="tag is-light">archived</span>
+          </h3>
+          <table className="table">
+            <thead>
+              <tr>
+                <th>Ticket No.</th>
+                <th>Date</th>
+                <th>Location</th>
+                <th>Issue</th>
+                <th>Status</th>
+              </tr>
+            </thead>
+            {tickets.filter(ticket => ticket.status === 'closed').map(tix => (
+              <tbody key={tix.id}>
+                <tr>
+                  <td>{tix.id}</td>
+                  <td>{tix.createdAt}</td>
+                  <td>{tix.location}</td>
+                  <td>{tix.issue}</td>
+                  <td>{tix.status}</td>
+                </tr>
+              </tbody>
+            ))}
+          </table>
+        </div>
+      </div>
     </div>
   )
 }
